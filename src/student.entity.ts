6e9b247444572b72ddea9cf801cf9fd6571155b9
paradoxfly <69152671+paradoxfly@users.noreply.github.com>
2023-07-26 19:01:14 +0100

    Feedback

--- src/student.entity.ts
+++ src/student.entity.ts
@@ -2,39 +2,39 @@ import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Student {
-	@PrimaryGeneratedColumn()
-	id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-	@Column()
-	name: string;
+  @Column()
+  name: string;
 
-	@Column()
-	registrationNumber: string;
+  @Column()
+  registrationNumber: string;
 
-	@Column()
-	mathematics: string;
+  @Column()
+  mathematics: string;
 
-	@Column()
-	englishLanguage: string;
+  @Column()
+  englishLanguage: string;
 
-	@Column()
-	biology: string;
+  @Column()
+  biology: string;
 
-	@Column()
-	chemistry: string;
+  @Column()
+  chemistry: string;
 
-	@Column()
-	physics: string;
+  @Column()
+  physics: string;
 
-	@Column()
-	french: string;
+  @Column()
+  french: string;
 
-	@Column()
-	technicalDrawing: string;
+  @Column()
+  technicalDrawing: string;
 
-	@Column()
-	workshop: string;
+  @Column()
+  workshop: string;
 
-	@Column('float')
-	gpa: number;
+  @Column('float')
+  gpa: number;
 }
